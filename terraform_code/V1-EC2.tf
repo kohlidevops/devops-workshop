e72de2a2f7b1cd88053656c408f4cc54438a2102
Lakshminarayanan <100069489+kohlidevops@users.noreply.github.com>
2024-06-29 13:26:36 +0530

    Update V1-EC2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-04f8d7ed2f1a54b14"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "devopstep1"
 }
